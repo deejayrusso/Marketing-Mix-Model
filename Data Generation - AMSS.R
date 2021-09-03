83017b3131336d37ee34c96e29693f967c4e926e
deejayrusso <djditucci@gmail.com>
2021-09-03 19:19:49 -0400

    Generated basic data and wrote to CSV

--- Data Generation - AMSS.R
+++ Data Generation - AMSS.R
@@ -1,9 +1,9 @@
 # Use devtools to pull amss library from Google's GitHub
-install.packages('devtools')
+#install.packages('devtools')
 library(devtools)
 
 # Install Aggregate Marketing System Simulator (amss) library from Google's GitHub
-install_github('google/amss')
+#install_github('google/amss')
 library(amss)
 
 # Simulate 4 years of weekly data
@@ -63,7 +63,7 @@ nat.mig.params <- list(
 budget.index <- rep(1:n.years, each = 52)
 
 
-# Define behavior of marketing interventions
+# Define behavior with marketing interventions
 
 # Television
 # Weekly flighting pattern:
@@ -100,6 +100,101 @@ params.tv <- list(
     favorability = tv.favorability.trans.mat))
 
 
+#Paid Search
+
+# set min and max CPC
+cpc.min <- 0.8
+cpc.max <- 1.1
+
+# Set paid search to be on 11 of 13 weeks with no spend cap while running
+# uncapped spend, shut off the first 2 of every 13 weeks
+spend.cap.fn <- function(time.index, budget, budget.index) {
+  if ((time.index %% 13) > 1) {
+    return(Inf)
+  } else {
+    return(0)
+  }
+}
+
+# Fix bid to maximum
+bid.fn <- function(time.index, per.capita.budget, budget.index) {
+  return(1.1)
+}
+
+# Set relationship between budget and spend as function of per capita budget
+# This way it is scalable to size of the population
+kwl.fn <- function(time.index, per.capita.budget, budget.index) {
+  return(4.5 * per.capita.budget)
+}
+
+
+# Define behavior with paid search interventions
+search.activity.trans.mat <- matrix(
+  c(0.05, 0.95, 0.00,  # starting state: inactive
+    0.00, 0.85, 0.15,  # starting state: exploratory
+    0.00, 0.00, 1.00),  # starting: purchase
+  nrow = length(kActivityStates), byrow = TRUE)
+
+search.favorability.trans.mat <- matrix(
+  c(1.0, 0.0, 0.0, 0.0, 0.0,  # unaware
+    0.0, 1.0, 0.0, 0.0, 0.0,  # negative
+    0.0, 0.0, 1.0, 0.0, 0.0,  # neutral
+    0.0, 0.0, 0.0, 1.0, 0.0,  # favorable
+    0.0, 0.0, 0.0, 0.0, 1.0),  # loyal
+  nrow = length(kFavorabilityStates), byrow = TRUE)
+
+# Paid search parameters
+params.search <- list(
+  audience.membership = list(activity = c(0.01, 0.3, 0.4)),
+  budget = (2.4e7 / n.years) * (1:n.years),
+  budget.index = budget.index,
+  spend.cap.fn = spend.cap.fn,
+  bid.fn = bid.fn,
+  kwl.fn = kwl.fn,
+  query.rate = 1,
+  cpc.min = cpc.min,
+  cpc.max = cpc.max,
+  ctr = list(activity = c(0.005, 0.08, 0.10)),
+  relative.effectiveness = c(0, 0.1, 1),
+  transition.matrices = list(
+    activity = search.activity.trans.mat,
+    favorability = search.favorability.trans.mat))
+
+
+# Sales
+
+sales.params <- list(
+  competitor.demand.max = list(loyalty = c(0.8, 0, 0.8)), # max % of switchers, advertiser-loyal, competitor-loyal
+  advertiser.demand.slope = list(favorability = rep(0, 5)),
+  advertiser.demand.intercept = list(
+    favorability = c(0.014, 0, 0.2, 0.3, 0.9)),
+  price = 80)
+
+
+# Generate simulated data
+
+sim.data <- SimulateAMSS(
+  time.n = time.n,
+  nat.mig.params = nat.mig.params,
+  media.names = c("tv", "search"),
+  media.modules = c(
+    `DefaultTraditionalMediaModule`,
+    `DefaultSearchMediaModule`),
+  media.params = list(params.tv, params.search),
+  sales.params = sales.params)
+
+# Remove 1st 52 weeks to ensure simulation had time to stabalize:
+burn.in.length <- 52
+final.year.end <- n.years * 52
+final.year.start <- final.year.end - 51
+observed.data <- sim.data$data[(burn.in.length + 1):final.year.end, ]
+
+
+# Write data to CSV
+dirname(rstudioapi::getSourceEditorContext()$path)
+fName = paste0(dirname(rstudioapi::getSourceEditorContext()$path),'/ObservedData.csv')
+
+write.csv(observed.data, file = fName)
 
 # References:
 "
