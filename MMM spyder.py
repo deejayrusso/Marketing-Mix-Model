6a34cf23a3453ea6f43ff681c0024df876aeb3d6
Dominick Russo <djditucci@gmail.com>
2021-11-28 14:09:04 -0500

    Delete MMM spyder.py

--- MMM spyder.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.read_csv('/Users/djdit/OneDrive - University of South Florida/Project/Data/firstfile.csv')
-print(df.head())
-print(df.shape)
-print(df.dtypes)
-print(df.describe())
